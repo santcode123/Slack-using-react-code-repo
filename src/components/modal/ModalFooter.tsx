1f786cb3b0de170608bebf8f0c3b2535ac1939ac
santosh <santosh.meena@sprinklr.com>
2022-01-21 21:29:37 +0530

    modal-enhancement-code

--- src/components/modal/ModalFooter.tsx
+++ src/components/modal/ModalFooter.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-
-//constants
-import { MODAL_TYPES } from '../../constants';
-
-const MODAL_SUBMIT_BUTTON_TEXT: { [name in typeof MODAL_TYPES[keyof typeof MODAL_TYPES]]: string } = {
-  [MODAL_TYPES.USER]: 'Create personal chat',
-  [MODAL_TYPES.CHANNEL]: 'Create a channel',
-  [MODAL_TYPES.APP]: 'Create a app',
-};
 export const ModalFooter = ({
-  modalType,
+  title,
   handleSubmit,
 }: {
-  modalType: typeof MODAL_TYPES[keyof typeof MODAL_TYPES];
+  title?: string;
   handleSubmit: (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }): React.ReactElement => {
   return (
     <div className="modal__footer">
       <button type="submit" className="form__submit__button" onClick={handleSubmit}>
-        {MODAL_SUBMIT_BUTTON_TEXT[modalType]}
+        {title}
       </button>
     </div>
   );
--- src/components/modal/ModalHeader.tsx
+++ src/components/modal/ModalHeader.tsx
@@ -1,31 +1,10 @@
-import React, { useCallback } from 'react';
-
-// constants
-import { MODAL_TYPES } from '../../constants';
-
-const MODAL_HEADER_TITLE: { [name in typeof MODAL_TYPES[keyof typeof MODAL_TYPES]]: string } = {
-  [MODAL_TYPES.USER]: 'Start a new personal chat',
-  [MODAL_TYPES.CHANNEL]: 'Create a new channel',
-  [MODAL_TYPES.APP]: 'Create a new app',
-};
-
-export const ModalHeader = ({
-  modalType,
-  close,
-}: {
-  modalType: typeof MODAL_TYPES[keyof typeof MODAL_TYPES];
-  close: () => void;
-}): React.ReactElement => {
-  const handleClose = useCallback(() => {
-    close();
-  }, [close]);
-
+export const ModalHeader = ({ title, close }: { title?: string; close: () => void }): React.ReactElement => {
   return (
     <div className="modal__header">
       <label>
-        <h3>{MODAL_HEADER_TITLE[modalType]}</h3>
+        <h3>{title}</h3>
       </label>
-      <button className="modal__close" onClick={handleClose}>
+      <button className="modal__close" onClick={close}>
         X
       </button>
     </div>
